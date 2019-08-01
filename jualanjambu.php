e18fafc4cbe516ab455a15c3c79df4512629ed1f
raihanfadzanaldy123 <fadzanraihan@gmail.com>
2019-08-01 16:59:35 +0700

    tugasawam

---
+++ jualanjambu.php
@@ -0,0 +1,13 @@
+<?php
+$kg1    = 15000;
+$dus    = 6;
+$jamdus = 5;
+$hardus = 2000;
+$hasil1 = ($kg1*$jamdus);
+$hasil2 = $hasil1*$dus;
+$hasil3 = $hardus*$dus;
+$hasilAkhir = $hasil2+$hasil3;
+
+echo "Hasil Seluruhnya : ".$hasilAkhir;
+
+?>
---
+++ soalUmur.php
@@ -0,0 +1,8 @@
+<?php
+$toni =0.5 ;
+$tama = 1;
+$tiniTama =14;
+$tiniToni = 34;
+echo "Ga tau Berapa, Nyerah saya lagi sakit kepala gamau maksain -Angsam";
+
+?>
